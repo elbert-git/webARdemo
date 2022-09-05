f0f3966a5e1a49fa82d2499eb6138874421fd626
elbert-git <elbertwithane@gmail.com>
2022-09-05 14:10:34 +0800

    finished sidebar

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { Experience } from "./three/experience";
 import { SideRoot } from "./sideRoot/sideRoot";
 
+// start 3d canvas
 const experience = new Experience();
+// sideRoot logic should only run on mobile
 const sideRoot = new SideRoot()
+
+
+
+// this is to tell the ts compiler to shut up
+console.log(experience, sideRoot);
--- src/sideRoot/sideRoot.ts
+++ src/sideRoot/sideRoot.ts
@@ -1,3 +1,54 @@
 export class SideRoot{
-  constructor(){}
+  elSlider:HTMLElement;
+  elSlideButton:HTMLElement;
+  elSVG:HTMLElement;
+  elButtonText:HTMLElement;
+  isDown:boolean;
+  elBannerArrays:NodeListOf<Element>
+  constructor(){
+    //* --- props
+    //elements 
+    this.elSlider = document.querySelector('#sideRootSlider')!;
+    this.elSlideButton = document.querySelector('#slideButton')!;
+    this.elSVG = document.querySelector('#slideButton svg')!;
+    this.elButtonText = document.querySelector('#slideButton div')!;
+    this.elBannerArrays = document.querySelectorAll('.banners')!;
+    // state
+    this.isDown = true;
+
+    // *button Event listener
+    console.log('add event listeners');
+    //slider buttons
+    this.elSlideButton.addEventListener('pointerdown', ()=>{this.toggle();})
+    // banner buttons
+    this.elBannerArrays.forEach((el, index) => {
+      el.addEventListener('pointerdown', ()=>{this.bannerClicked(index)})
+    });
+  }
+
+  toggle():void{
+    console.log('toggle');
+    // toggle slide up and down
+    this.elSlider.classList.toggle('sideRootDown');
+
+    // change text
+    if(this.elButtonText.innerHTML === "Slide Up"){
+      this.elButtonText.innerHTML = "Slide Down";
+    }else{
+      this.elButtonText.innerHTML = "Slide Up";
+    }
+
+    // rotate svg
+    this.elSVG.classList.toggle('svgRotate');
+
+    //update state
+    this.isDown = !this.isDown;
+  }
+
+  bannerClicked(index:number):void{
+    console.log('clicked banner ', index);
+
+    //make banne slide down
+    this.toggle();
+  }
 }
